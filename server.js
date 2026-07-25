404a1ea1e11774437793a9a52a59455c02ccd609
HAGLA <bienhagla@gmail.com>
2026-07-25 18:03:34 +0100

    Fix: Placer backtick au bon endroit (const query = backtick)

--- server.js
+++ server.js
@@ -74,14 +74,13 @@ app.post('/parcels', async (req, res) => {
       status 
     } = req.body;
 
-     `const query =
+     const query = `
       INSERT INTO colis 
       (de, a, prix, nom_receptionnaire, prenom_receptionnaire, contact_receptionnaire, 
        adresse_livraison, description_colis, photo_colis, status, created_at, updated_at)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, NOW(), NOW())
       RETURNING *
     `;
-
     const values = [
       de,
       a,
